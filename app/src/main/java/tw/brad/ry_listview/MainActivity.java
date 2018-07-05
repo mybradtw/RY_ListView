cf504060a530745ba2cf6d11ce574f0b900e6d55
Brad Chao <brad@brad.tw>
2018-07-05 10:51:38 +0800

    v1

---
+++ app/src/main/java/tw/brad/ry_listview/MainActivity.java
@@ -0,0 +1,19 @@
+package tw.brad.ry_listview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void add(View view) {
+    }
+    public void del(View view) {
+    }
+}
